e8fc85e93276cea2eb19da0a2569e36ac1d3b413
damiepsz3 <damiepsz3@gmail.com>
2017-11-19 12:40:04 -0500

    feat: :sparkles: working with query and mutation

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Home.css';
+import CreateLink from '../CreateLink';
+
+class Home extends Component {
+  render() {
+    return <CreateLink />;
+  }
+}
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cja70kbfz0vtn0125zvluiqu5' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Home />
+  </ApolloProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
