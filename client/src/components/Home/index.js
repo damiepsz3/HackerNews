f911ad2042633aac2320cc51c8a1bca967f8d019
damienepsztein <damiepsz3@gmail.com>
2017-11-20 14:18:17 -0500

    feat: :sparkles: search text in url or content

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -4,6 +4,7 @@ import CreateLink from '../CreateLink';
 import LinkList from '../LinkList';
 import Header from '../Header';
 import Login from '../Login';
+import Search from '../Search';
 import { Switch, Route } from 'react-router-dom';
 
 class Home extends Component {
@@ -15,6 +16,7 @@ class Home extends Component {
           <Switch>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/create' component={CreateLink}/>
+            <Route exact path='/search' component={Search}/>
             <Route exact path='/' component={LinkList}/>
           </Switch>
         </div>
