0d9fafd0fd87508e5004bceaf344b55d28dcdc30
damienepsztein <damiepsz3@gmail.com>
2017-11-20 11:50:33 -0500

    feat: :sparkles: Sign-up and Sign-in

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -3,6 +3,7 @@ import './Home.css';
 import CreateLink from '../CreateLink';
 import LinkList from '../LinkList';
 import Header from '../Header';
+import Login from '../Login';
 import { Switch, Route } from 'react-router-dom';
 
 class Home extends Component {
@@ -12,8 +13,9 @@ class Home extends Component {
         <Header />
         <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path='/' component={LinkList}/>
+            <Route exact path='/login' component={Login}/>
             <Route exact path='/create' component={CreateLink}/>
+            <Route exact path='/' component={LinkList}/>
           </Switch>
         </div>
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,26 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
+import { GC_AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
 
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cja70kbfz0vtn0125zvluiqu5' })
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 
